1b7d31abbe14f0beb549206fd15f0171f12aa872
jenildesai25 <jenil.desai25@gmail.com>
2018-12-10 02:31:21 -0600

    completed data handler for genome dataset

--- Task_A_Jenil.py
+++ Task_A_Jenil.py
@@ -1,16 +1,22 @@
-import numpy as np
 import pandas as pd
+import sys
+import os
+from contextlib import redirect_stdout
 
 
 def load_data(file_path):
     try:
-        if 'Genome' in file_path:
+        if 'nome' in file_path:
             data_frame = pd.read_csv(file_path, header=None, sep=",")
-            class_numbers = data_frame.iloc[:1, :]
-            only_data_frame = data_frame.iloc[1:, :]
-            # print('Data has been cleaned')
-            return data_frame, class_numbers, only_data_frame
-        elif 'Iris' in file_path:
+            data_frame_T = data_frame.T
+            class_numbers = data_frame_T[0]
+            data_frame_T = data_frame_T.iloc[:, 1:].reset_index(drop=True)
+            data_frame_T.index = class_numbers.index
+            new_data_frame = pd.concat([data_frame_T, class_numbers], axis=1)
+            new_data_frame = new_data_frame.rename(columns={new_data_frame.columns[-1]: "class numbers"})
+            print('Data has been cleaned and rearranged for F-test algorithm')
+            return new_data_frame, class_numbers, data_frame_T
+        elif 'ris' in file_path:
             data_frame = pd.read_csv(file_path, header=None, sep=",")
             class_numbers = data_frame.iloc[:, -1]
             only_data_frame = data_frame.iloc[:, :-1]
@@ -25,17 +31,13 @@ def load_data(file_path):
 def f_test_for_iris(data):
     try:
         # column wise mean of the dataframe.
-        # TODO start for loop for groups. here we have 3 groups and 4 features.
+        os.remove('jenil_result.txt')
         data_mean = data.iloc[:, :-1].mean()
         total_class_numbers = set(data.iloc[:, -1])
-        # data_frame_size = int(data.iloc[:, :-1].size)
         data_mean_float = []
         for i in data_mean:
             data_mean_float.append(float(i))
-        # explained_variance = 0
-        k = len(data.columns.values)
         container_for_groups = {}
-        # overall_data_mean = np.mean(data_mean_float)
         for i, value in enumerate(data.iloc[:, :-1].columns.values):
             container_for_groups[value] = []
             variance_values = []
@@ -49,49 +51,35 @@ def f_test_for_iris(data):
                 container_for_groups[value].append('mean of {} {}st/nd part is {} '.format(value, j, mean_of_class))
                 new_data_frame_with_subtracting_mean = pd.DataFrame(class_data_frame[value] - mean_of_class)
                 square_of_data_frame = pd.DataFrame(new_data_frame_with_subtracting_mean[value] ** 2)
-                # TODO divide next line by 49. (class data frame size - 1)
+
                 variance = float(square_of_data_frame[value].sum() / float(int(class_data_frame.size) - 1))
                 variance_values.append(variance)
-                # float(sum_of_squares_within_groups + total)
                 container_for_groups[value].append('sum of squares within groups is {} after {}th/nd loop '.format(variance_values, j))
             mean_of_whole_column = data_mean_float[i]
             container_for_groups[value].append('mean of {}th/nd column is{}'.format(i, mean_of_whole_column))
             data_frame = pd.DataFrame(data[value])
-            # zip_of_average_and_variance = zip(average_values, variance_values)
-            # f = 0
-
             value_1 = 0
             for j in range(len(total_class_numbers)):
                 value_1 = value_1 + int(data_frame_size[j]) * ((average_values[j] - mean_of_whole_column) ** 2)
-            numer = value_1 / (len(total_class_numbers) - 1)
-            # print('container is', numer)
-
+            numerator = value_1 / (len(total_class_numbers) - 1)
             value_2 = 0
             for k in range(len(variance_values)):
                 size = int(data_frame_size[k]) - 1
                 value_2 = value_2 + (size * variance_values[k])
-            den = float(value_2) / (len(data_frame) - len(total_class_numbers))
-            final_result = numer / den
+            denominator = float(value_2) / (len(data_frame) - len(total_class_numbers))
+            if not numerator or not denominator:
+                final_result = float(0)
+            if numerator and denominator:
+                final_result = numerator / denominator
             print('{} f={}'.format(value, final_result))
-            # for average, variance in zip(average_values, variance_values):
-            #     temp = class_data_frame.size()
-            # data_frame_after_subtracting_mean_of_whole_column = pd.DataFrame(data_frame[value] - mean_of_whole_column)
-            # square_of_data_frame_after_subtracting_mean_of_whole_column = pd.DataFrame(data_frame_after_subtracting_mean_of_whole_column[value] ** 2)
-            # sum_of_square_of_data_frame_after_subtracting_mean_of_whole_column = float(square_of_data_frame_after_subtracting_mean_of_whole_column[value].sum())
-            # sum_of_square_between_groups = float(sum_of_square_of_data_frame_after_subtracting_mean_of_whole_column - sum_of_squares_within_groups)
-            # container_for_groups[value].append('sum of square between groups is {}'.format(sum_of_square_between_groups))
-            # sum_of_square_between_groups_divide_by_degrees_of_freedom = float(sum_of_square_between_groups / len(total_class_numbers) - 1)
-            # container_for_groups[value].append('sum of square between group divide by degrees of freedom is {}'.format(sum_of_square_between_groups_divide_by_degrees_of_freedom))
-            # degrees_of_freedom_for_within_groups = float(data_frame.size - len(total_class_numbers))
-            # sum_of_square_within_groups_divide_by_degrees_of_freedom = float(sum_of_squares_within_groups / degrees_of_freedom_for_within_groups)
-            # container_for_groups[value].append('sum of square within groups divide by degrees of freedom is {}'.format(sum_of_square_within_groups_divide_by_degrees_of_freedom))
-            # f_test_result = float(sum_of_square_between_groups_divide_by_degrees_of_freedom / sum_of_square_within_groups_divide_by_degrees_of_freedom)
+            with open('jenil_result.txt', 'a') as f:
+                with redirect_stdout(f):
+                    print(value, final_result)
 
-            # print('{} f= {}'.format(value, f_test_result))
-            # for key, values in container_for_groups.items():
-            #     print(key, values)
     except Exception as e:
-        print(e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
 
 
 if __name__ == '__main__':
