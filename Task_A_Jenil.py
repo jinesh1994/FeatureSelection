46354e071641e8acbe0d53cb00698102a622887f
jenildesai25 <jenil.desai25@gmail.com>
2018-12-10 14:42:11 -0600

    added code for generating user selected row

--- Task_A_Jenil.py
+++ Task_A_Jenil.py
@@ -28,10 +28,10 @@ def load_data(file_path):
         print(e)
 
 
-def f_test_for_iris(data):
+def f_test_for_iris(data, label_values):
     try:
         # column wise mean of the dataframe.
-        os.remove('jenil_result.txt')
+        # os.remove('jenil_result.txt')
         data_mean = data.iloc[:, :-1].mean()
         total_class_numbers = set(data.iloc[:, -1])
         data_mean_float = []
@@ -39,7 +39,6 @@ def f_test_for_iris(data):
             data_mean_float.append(float(i))
         container_for_groups = {}
         for i, value in enumerate(data.iloc[:, :-1].columns.values):
-            container_for_groups[value] = []
             variance_values = []
             average_values = []
             data_frame_size = []
@@ -48,15 +47,15 @@ def f_test_for_iris(data):
                 data_frame_size.append(class_data_frame.size)
                 mean_of_class = float(class_data_frame.mean())
                 average_values.append(mean_of_class)
-                container_for_groups[value].append('mean of {} {}st/nd part is {} '.format(value, j, mean_of_class))
+
                 new_data_frame_with_subtracting_mean = pd.DataFrame(class_data_frame[value] - mean_of_class)
                 square_of_data_frame = pd.DataFrame(new_data_frame_with_subtracting_mean[value] ** 2)
 
                 variance = float(square_of_data_frame[value].sum() / float(int(class_data_frame.size) - 1))
                 variance_values.append(variance)
-                container_for_groups[value].append('sum of squares within groups is {} after {}th/nd loop '.format(variance_values, j))
+
             mean_of_whole_column = data_mean_float[i]
-            container_for_groups[value].append('mean of {}th/nd column is{}'.format(i, mean_of_whole_column))
+
             data_frame = pd.DataFrame(data[value])
             value_1 = 0
             for j in range(len(total_class_numbers)):
@@ -72,10 +71,29 @@ def f_test_for_iris(data):
             if numerator and denominator:
                 final_result = numerator / denominator
             print('{} f={}'.format(value, final_result))
+            container_for_groups[value] = final_result
             with open('jenil_result.txt', 'a') as f:
                 with redirect_stdout(f):
                     print(value, final_result)
+        container_for_groups_sorted_keys = sorted(container_for_groups.items(), key=lambda t: t[1], reverse=True)
+        select_rows = int(input('\n please select rows you want: '))
+        get_top_data_as_per_user(data, container_for_groups_sorted_keys, select_rows, label_values)
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
 
+
+def get_top_data_as_per_user(all_data, dictionary, input_value, labels):
+    try:
+        data_frame = pd.DataFrame()
+        for i in range(input_value):
+            temp = all_data[dictionary[i][0]]
+            data_frame.insert(i, i, temp)
+        data_frame.index = labels.index
+        new_data_frame = pd.concat([data_frame, labels], axis=1)
+        new_data_frame = new_data_frame.rename(columns={new_data_frame.columns[-1]: "class numbers"})
+        new_data_frame.to_csv('jenil_top_{}_data.csv'.format(input_value), sep=',', header=None, index=False)
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -85,4 +103,4 @@ def f_test_for_iris(data):
 if __name__ == '__main__':
     user_input = input('please provide file name of full file path: ')
     data, labels, data_without_labels = load_data(user_input)
-    f_test_for_iris(data=data)
+    f_test_for_iris(data=data, label_values=labels)
